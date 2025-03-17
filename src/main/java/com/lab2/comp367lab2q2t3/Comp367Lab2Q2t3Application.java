57d8b5ebcb134f7c8b5af8551c17a044c9878c56
Hi-kue <lofi.audit@gmail.com>
2025-03-16 20:59:57 -0400

    base: initial commit

---
+++ src/main/java/com/lab2/comp367lab2q2t3/Comp367Lab2Q2t3Application.java
@@ -0,0 +1,11 @@
+package com.lab2.comp367lab2q2t3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp367Lab2Q2t3Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Comp367Lab2Q2t3Application.class, args);
+    }
+}
---
+++ src/test/java/com/lab2/comp367lab2q2t3/Comp367Lab2Q2t3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab2.comp367lab2q2t3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp367Lab2Q2t3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
